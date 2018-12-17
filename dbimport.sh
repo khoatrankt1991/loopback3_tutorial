b25da6a6e1c23572dae278a76d2d2c3303acdb23
khoatrankt1991 <khoatran.kt1991@gmail.com>
2018-12-17 23:10:19 +0700

    Update dbimport.sh

--- dbimport.sh
+++ dbimport.sh
@@ -8,9 +8,9 @@ fi
 
 MONGO_CONTAINER="$(docker-compose ps -q mongo)"
 
-docker exec -ti $MONGO_CONTAINER bash -c "mkdir -p /tmp && rm -rf /tmp/dump"
+docker exec -i $MONGO_CONTAINER bash -c "mkdir -p /tmp && rm -rf /tmp/dump"
 docker cp "$1" $MONGO_CONTAINER:/tmp/dump
-docker exec -ti $MONGO_CONTAINER bash -c \
+docker exec -i $MONGO_CONTAINER bash -c \
   "cd /tmp/dump && mongorestore --db lb3ex ."
 
 echo "Done."
