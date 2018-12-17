8d6aba4e10266ce81e51cd8fb1923ec36761dff8
khoatrankt1991 <khoatran.kt1991@gmail.com>
2018-12-17 10:18:17 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 echo "Start deployment on server"
 
-ssh root@139.99.202.246 "cd ./projects/lb3api && git pull && npm install && ./stop.sh && docker-compose build && ./start.sh"
+ssh root@139.99.202.246 "cd ./projects/lb3api && ./stop.sh && git pull && npm install && docker-compose build && ./start.sh"
 
 echo "Done"
